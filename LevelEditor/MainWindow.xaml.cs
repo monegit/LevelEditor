4bff56fb76cb4122e8f30563ab5264734aa7a7e6
종원 박 <pjw970220@gmail.com>
2022-05-17 02:52:46 +0900

    Asset(Tile, Object, Etc...) 불러오기

--- LevelEditor/MainWindow.xaml.cs
+++ LevelEditor/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace LevelEditor
 {
@@ -12,17 +14,38 @@ namespace LevelEditor
         public MainWindow()
         {
             InitializeComponent();
+
+            string path = $@"{Directory.GetCurrentDirectory()}\Asset";
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("dd");
+                foreach (var a in Tile.Asset.Load.getAssetList($@"{path}\Tile"))
+                {
+                    var assetImage = new Image()
+                    {
+                        Margin = new Thickness(5),
+                        Width = Constant.TILE_SIZE,
+                        Height = Constant.TILE_SIZE,
+                    };
+
+                    assetImage.Source = new BitmapImage(new Uri(a, UriKind.Absolute));
+                    AssetList.Children.Add(assetImage);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+            }
         }
 
         private void Apply_Click(object sender, RoutedEventArgs e)
         {
-            int tileSize = 30;
             var canvas = new Grid() { Margin = new Thickness(20) };
 
             if (int.TryParse(WidthValue.Text, out int _widthValue))
                 for (int i = 0; i < _widthValue; i++)
                 {
-                    var column = new ColumnDefinition() { Width = new GridLength(tileSize, GridUnitType.Pixel) };
+                    var column = new ColumnDefinition() { Width = new GridLength(Constant.TILE_SIZE, GridUnitType.Pixel) };
 
                     canvas.ColumnDefinitions.Add(column);
                 }
@@ -30,7 +53,7 @@ namespace LevelEditor
             if(int.TryParse(HeightValue.Text,out int _heightValue))
                 for (int i = 0; i < _heightValue; i++)
                 {
-                    var row = new RowDefinition() { Height = new GridLength(tileSize, GridUnitType.Pixel) };
+                    var row = new RowDefinition() { Height = new GridLength(Constant.TILE_SIZE, GridUnitType.Pixel) };
 
                     canvas.RowDefinitions.Add(row);
                 }
@@ -48,52 +71,12 @@ namespace LevelEditor
                 }
 
             MapScroll.Content = canvas;
-
-
-            /*bool isWidth = int.TryParse(WidthValue.Text, out int _widthValue);
-
-            if (isWidth)
-                for (int w = 0; w < _widthValue; w++)
-                {
-
-                    int _heightValue;
-                    bool isHeight = int.TryParse(HeightValue.Text, out _heightValue);
-
-                    if (isHeight)
-                        for (int h = 0; h < _heightValue; h++)
-                        {
-                            var row = new RowDefinition() { Height = new GridLength(tileSize, GridUnitType.Pixel) };
-                            var column = new ColumnDefinition() { Width = new GridLength(tileSize, GridUnitType.Pixel) };
-
-                            MapCanvas.RowDefinitions.Add(row);
-                            MapCanvas.ColumnDefinitions.Add(column);
-
-                            var tile = new Tile.Tile();
-                            tile.SetPosition(w, h);
-
-                            Grid.SetRow(tile, h);
-                            Grid.SetColumn(tile, w);
-                            MapCanvas.Children.Add(tile);
-                        }
-                    else
-                    {
-                        MessageBox.Show("Height is not Number", "Error");
-                        HeightValue.Focus();
-                        break;
-                    }
-                }
-            else
-            {
-                MessageBox.Show("Width is not Number", "Error");
-                WidthValue.Focus();
-            }*/
-            //MessageBox.Show(MapCanvas.ActualWidth.ToString());
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
+            //TODO: dialog로 저장
             Document.Document.Save(@"C:\Users\pjw97\Desktop\a.txt");
-            //File.WriteAllText(@"C:\Users\pjw97\Desktop\a.txt", Document.Document.map.ToString());
         }
     }
 }
---
+++ LevelEditor/Tile/Asset/Load.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LevelEditor.Tile.Asset
+{
+    internal class Load
+    {
+        public static List<string> getAssetList(string path)
+        {
+            var files = new List<string>();
+
+            foreach(var file in Directory.GetFiles(path))
+                files.Add(file);
+
+            return files;
+        }
+    }
+}
